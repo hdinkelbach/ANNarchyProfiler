1848cdb66fdcdea0e942b3b97bdbf9fd32d371a6
king-ton <tfreitag93@gmail.com>
2017-01-12 15:18:07 +0100

    Multi-File-Support for different number of threads
    - Add: Load multiple xml files into the Application at one time
    - Add: Choose between those data and load into the widgets

--- Analyzer.py
+++ Analyzer.py
@@ -29,50 +29,6 @@ try:
 except AttributeError:
     def _fromUtf8(s):
         return s
-
-class Analyzer(QtGui.QTabWidget):
-    """
-    Main widget. Choose between pie and error chart.
-    Hold data from loaded xml-file.
-    """
-    def __init__(self, parent):
-        """
-        Init function. Create empty data-container.
-        """
-        QtGui.QTabWidget.__init__(self, parent)
-        self._data_container = DataContainer()
-        
-    @QtCore.pyqtSlot()
-    def load_data(self):
-        """
-        Open file-dialog and import data into container.
-        Set data in all tabs.
-        """
-        fname = QtGui.QFileDialog.getOpenFileName(self, 'Open data file', '.', '*.xml')
-
-        if fname:
-            self.set_data(fname)
-            
-    def set_data(self, fname):
-        self._data_container.load_data(fname)
-
-        self.emit(QtCore.SIGNAL(_fromUtf8("setPieChartTree(PyQt_PyObject)")), self._data_container)
-        self.emit(QtCore.SIGNAL(_fromUtf8("setErrorbarChartTree(PyQt_PyObject)")), self._data_container)
-        
-     
-    @QtCore.pyqtSlot()
-    def load_pie_chart(self):
-        """
-        Send data-container to PieChartTree to change Chart.
-        """
-        self.emit(QtCore.SIGNAL(_fromUtf8("setPieChart(PyQt_PyObject)")), self._data_container)
-    
-    @QtCore.pyqtSlot()
-    def load_errorbar_chart(self):
-        """
-        Send data-container to ErrorbarChartTree to change Chart.
-        """
-        self.emit(QtCore.SIGNAL(_fromUtf8("setErrorbarChart(PyQt_PyObject)")), self._data_container)
             
 class PieChartTree(QtGui.QTreeWidget):
     """
@@ -113,72 +69,73 @@ class PieChartTree(QtGui.QTreeWidget):
         Signals:
             * currentItemChanged(QTreeWidgetItem, QTreeWidgetItem) emited from PieChartTree
         """
-        topIdx = self.invisibleRootItem().indexOfChild(current)
-        if topIdx != -1: # top element? (Network)
-            values = self._data.values_by_type(topIdx, "net")
-            
-            # net-step = overhead + net-proj_step + net-psp + net-neur_step
-            overhead = values["step"]["mean"] - (values["proj_step"]["mean"] + values["psp"]["mean"] + values["neur_step"]["mean"])
+        if current:
+            topIdx = self.invisibleRootItem().indexOfChild(current)
+            if topIdx != -1: # top element? (Network)
+                values = self._data.values_by_type(topIdx, "net")
                 
-            data = [
-                    ["Overhead\n(" + "%.4f" % overhead + ")", "%.4f" % overhead],
-                    ["proj_step\n(" + "%.4f" % values["proj_step"]["mean"] + ")", "%.4f" % values["proj_step"]["mean"]],
-                    ["psp\n(" + "%.4f" % values["psp"]["mean"] + ")", "%.4f" % values["psp"]["mean"]],
-                    ["neur_step\n(" + "%.4f" % values["neur_step"]["mean"] + ")", "%.4f" % values["neur_step"]["mean"]]
-                ]
-            self.emit(QtCore.SIGNAL(_fromUtf8("drawPieChart(PyQt_PyObject,PyQt_PyObject,PyQt_PyObject)")), data, current.text(0) + " (in ms)", True)
-        else:
-            childIdx = current.parent().indexOfChild(current)
-            topIdx = self.invisibleRootItem().indexOfChild(current.parent())
-            if topIdx != -1: # First child of Network?
-                
-                if childIdx == 0:
-                    neur_step = self._data.values_by_function(topIdx, "net", "neur_step")
-                    func_data = self._data.values_by_function(topIdx, "pop", "step")
-
-                    overhead = neur_step["mean"]
-                    values = []
-                    for key,value in func_data.items():
-
-                        overhead -= value["mean"]
-                        values.append(["pop" + str(key), "%.4f" % value["mean"]])
+                # net-step = overhead + net-proj_step + net-psp + net-neur_step
+                overhead = values["step"]["mean"] - (values["proj_step"]["mean"] + values["psp"]["mean"] + values["neur_step"]["mean"])
                     
-                    values.append(["overhead", "%.4f" % overhead])
+                data = [
+                        ["Overhead\n(" + "%.4f" % overhead + ")", "%.4f" % overhead],
+                        ["proj_step\n(" + "%.4f" % values["proj_step"]["mean"] + ")", "%.4f" % values["proj_step"]["mean"]],
+                        ["psp\n(" + "%.4f" % values["psp"]["mean"] + ")", "%.4f" % values["psp"]["mean"]],
+                        ["neur_step\n(" + "%.4f" % values["neur_step"]["mean"] + ")", "%.4f" % values["neur_step"]["mean"]]
+                    ]
+                self.emit(QtCore.SIGNAL(_fromUtf8("drawPieChart(PyQt_PyObject,PyQt_PyObject,PyQt_PyObject)")), data, current.text(0) + " (in ms)", True)
+            else:
+                childIdx = current.parent().indexOfChild(current)
+                topIdx = self.invisibleRootItem().indexOfChild(current.parent())
+                if topIdx != -1: # First child of Network?
                     
-                if childIdx == 1:
-                    proj_step = self._data.values_by_function(topIdx, "net", "proj_step")
-                    func_data = self._data.values_by_function(topIdx, "proj", "step")
-
-                    overhead = proj_step["mean"]
-                    values = []
-                    for key,value in func_data.items():
-
-                        overhead -= value["mean"]
-                        values.append(["proj" + str(key), "%.4f" % value["mean"]])
+                    if childIdx == 0:
+                        neur_step = self._data.values_by_function(topIdx, "net", "neur_step")
+                        func_data = self._data.values_by_function(topIdx, "pop", "step")
+    
+                        overhead = neur_step["mean"]
+                        values = []
+                        for key,value in func_data.items():
+    
+                            overhead -= value["mean"]
+                            values.append(["pop" + str(key), "%.4f" % value["mean"]])
+                        
+                        values.append(["overhead", "%.4f" % overhead])
+                        
+                    if childIdx == 1:
+                        proj_step = self._data.values_by_function(topIdx, "net", "proj_step")
+                        func_data = self._data.values_by_function(topIdx, "proj", "step")
+    
+                        overhead = proj_step["mean"]
+                        values = []
+                        for key,value in func_data.items():
+    
+                            overhead -= value["mean"]
+                            values.append(["proj" + str(key), "%.4f" % value["mean"]])
+                        
+                        values.append(["overhead", "%.4f" % overhead])
+                        
+                    if childIdx == 2:
+                        net_psp = self._data.values_by_function(topIdx, "net", "psp")
+                        func_data = self._data.values_by_function(topIdx, "proj", "psp")
+    
+                        overhead = net_psp["mean"]
+                        values = []
+                        for key,value in func_data.items():
+    
+                            overhead -= value["mean"]
+                            values.append(["proj" + str(key), "%.4f" % value["mean"]])
+                        
+                        values.append(["overhead", "%.4f" % overhead])
+                    #values = self._data.values_by_function(topIdx, current.text(0))
                     
-                    values.append(["overhead", "%.4f" % overhead])
+                    # calc overhead
+                    #overhead = values[0][1]
+                    #for i in xrange(1, len(values)):
+                        #overhead -= values[i][1]
+                    #values[0] = ["overhead", "%.4f" % overhead]
                     
-                if childIdx == 2:
-                    net_psp = self._data.values_by_function(topIdx, "net", "psp")
-                    func_data = self._data.values_by_function(topIdx, "proj", "psp")
-
-                    overhead = net_psp["mean"]
-                    values = []
-                    for key,value in func_data.items():
-
-                        overhead -= value["mean"]
-                        values.append(["proj" + str(key), "%.4f" % value["mean"]])
-                    
-                    values.append(["overhead", "%.4f" % overhead])
-                #values = self._data.values_by_function(topIdx, current.text(0))
-                
-                # calc overhead
-                #overhead = values[0][1]
-                #for i in xrange(1, len(values)):
-                    #overhead -= values[i][1]
-                #values[0] = ["overhead", "%.4f" % overhead]
-                
-                self.emit(QtCore.SIGNAL(_fromUtf8("drawPieChart(PyQt_PyObject,PyQt_PyObject,PyQt_PyObject)")), values, current.text(0) + " (in ms)", False)
+                    self.emit(QtCore.SIGNAL(_fromUtf8("drawPieChart(PyQt_PyObject,PyQt_PyObject,PyQt_PyObject)")), values, current.text(0) + " (in ms)", False)
             
             
 class ErrorbarChartTree(QtGui.QTreeWidget):
@@ -216,8 +173,9 @@ class ErrorbarChartTree(QtGui.QTreeWidget):
         Signals:
             * currentItemChanged(QTreeWidgetItem, QTreeWidgetItem) emited from ErrorbarChartTree
         """
-        obj = str(current.text(0)).split(" - ")
-        mean_values = self._data.values_each_test(obj[0], obj[1], "mean")
-        std_values = self._data.values_each_test(obj[0], obj[1], "std")
-        
-        self.emit(QtCore.SIGNAL(_fromUtf8("drawErrorbarChart(PyQt_PyObject, PyQt_PyObject)")), mean_values, std_values)
+        if current:
+            obj = str(current.text(0)).split(" - ")
+            mean_values = self._data.values_each_test(obj[0], obj[1], "mean")
+            std_values = self._data.values_each_test(obj[0], obj[1], "std")
+            
+            self.emit(QtCore.SIGNAL(_fromUtf8("drawErrorbarChart(PyQt_PyObject, PyQt_PyObject)")), mean_values, std_values)
--- ProfilerWindow.py
+++ ProfilerWindow.py
@@ -22,8 +22,10 @@
 #
 #==============================================================================
 from PyQt4.QtCore import pyqtSlot, SIGNAL
-from PyQt4.QtGui import QMainWindow
+from PyQt4.QtGui import QFileDialog, QMainWindow, QMessageBox
 from PyQt4.uic import loadUi
+
+from DataContainer import DataContainer
 
 class ProfilerWindow(QMainWindow):
     """
@@ -37,16 +39,88 @@ class ProfilerWindow(QMainWindow):
         self.ui = loadUi("ProfilerWindow.ui")
         
         # actions menubar
-        self.ui.connect(self.ui.btnRunMeasurement, SIGNAL("activated()"), self.load_run_dialog)
-        self.ui.connect(self.ui.btnSave, SIGNAL("activated()"), self.save_chart)
+        self.ui.connect(self.ui.btnLoadData, SIGNAL('activated()'), self.load_data_dialog)
+        self.ui.connect(self.ui.btnRunMeasurement, SIGNAL('activated()'), self.load_run_dialog)
+        self.ui.connect(self.ui.btnSave, SIGNAL('activated()'), self.save_chart)
+        
+        # action combobox
+        self.ui.connect(self.ui.cmbThread, SIGNAL('currentIndexChanged(int)'), self.change_combobox)
         
         # set class variables 
         self._data = {}
+    
+    def show(self):
+        self.ui.show()
+        
+    def add_data(self, data):
+        """
+        Add a DataContainer instance to test-data.
+        Ask for overwriting if a test with same number of threads exists.
+        """
+        if self._data.has_key(data.num_threads()):
+            msg = QMessageBox()
+            msg.setText("Data from the same number of threads is set. Want to overwrite?")
+            msg.setIcon(QMessageBox.Warning)
+            msg.setStandardButtons(QMessageBox.No | QMessageBox.Yes)
+            msg.setDefaultButton(QMessageBox.Yes)
+            if msg.exec_() != QMessageBox.Yes:
+                return
+        
+        self._data[data.num_threads()] = data
+        self.update_combobox()
+        
+        
+    #==============================================================================
+    # actions for the combobox for choosing test-data
+    #==============================================================================
+    
+    @pyqtSlot()
+    def change_combobox(self, idx):
+        """
+        Update TreeViews if combobox changed.
+        
+        Signals:
+            * currentIndexChanged(int) emitted from cmbThread
+        """
+        if self.ui.cmbThread.itemData(idx).toInt()[1]:
+            key = self.ui.cmbThread.itemData(idx).toInt()[0]
+            self.ui.PieChartTree.load_data(self._data[key])
+            self.ui.ErrorbarChartTree.load_data(self._data[key])
+    
+    def update_combobox(self):
+        """
+        Update the items of the combobox from test data
+        """
+        self.ui.cmbThread.clear()
+        for key in self._data:
+            self.ui.cmbThread.addItem(str(key) + " Threads ", key)
+    
+    
+    #==============================================================================
+    # actions for the buttons in the menubar
+    #==============================================================================
+    
+    @pyqtSlot()    
+    def load_data_dialog(self):
+        """
+        Open file-dialog to choose multiple files. Add data to container and set it in application.
+        
+        Signals:
+            * activated() emitted from btnLoadData in menubar
+        """
+        fnames = QFileDialog.getOpenFileNames(self, 'Open data file', '.', '*.xml')
+
+        for fname in fnames:
+            data = DataContainer()
+            data.load_data(fname)
+            self.add_data(data)
+        
     
     @pyqtSlot()
     def load_run_dialog(self):
         """
         Shows a dialog to enter data for a ANNarchy profile run.
+        Measurement data will be load into the application.
         
         Signals:
             * activated() emitted from btnRunMeasurement in menubar
@@ -57,7 +131,10 @@ class ProfilerWindow(QMainWindow):
         if(script != ""):
             os.system("cd " + str(path))
             os.system("python " + str(script) + " --profile --profile_out=" + str(path) + "/measurement.xml " + str(args))
-            self.ui.AnalyzerWidget.set_data(str(path) + "/measurement.xml")
+            
+            data = DataContainer()
+            data.load_data(str(path) + "/measurement.xml")
+            self.add_data(data)
     
     @pyqtSlot()
     def save_chart(self):
@@ -81,5 +158,3 @@ class ProfilerWindow(QMainWindow):
             if(fname):
                 figure.savefig(str(fname))
     
-    def show(self):
-        self.ui.show()
